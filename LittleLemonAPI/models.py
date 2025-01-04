7699c4fca403ec3d1e44d8310d62a73ff1935df9
Mahder Tesfaye Abebe <mahder.tesfaye@aait.edu.et>
2025-01-04 16:17:32 +0300

    edited errors in syntax

--- LittleLemonAPI/models.py
+++ LittleLemonAPI/models.py
@@ -5,11 +5,11 @@ from django.contrib.auth.models import User
 
 class Category(models.Model):
     slug=models.SlugField()
-    title=models.CharField(max_length=255, dp_index=True)
+    title=models.CharField(max_length=255, db_index=True)
 
 class MenuItems(models.Model):
     title=models.CharField(max_length=255, db_index=True)
-    price=models.DecimalField(max_digits=6,decimal_palces=2,db_index=True)
+    price=models.DecimalField(max_digits=6,decimal_places=2,db_index=True)
     featured=models.BooleanField(db_index=True)
     category=models.ForeignKey(Category, on_delete=models.PROTECT)
     
@@ -17,27 +17,27 @@ class Cart(models.Model):
     user=models.ForeignKey(User, on_delete=models.CASCADE)
     menuitem=models.ForeignKey(MenuItems, on_delete=models.CASCADE)
     quantity=models.SmallIntegerField()
-    unit_price=models.DecimalField(max_digits=6,decimal_palces=2)
-    price=models.DecimalField(max_digits=6, decimal_palces=2)
+    unit_price=models.DecimalField(max_digits=6,decimal_places=2)
+    price=models.DecimalField(max_digits=6, decimal_places=2)
     
     class Meta:
-        unique_together=('user','menu_item')
+        unique_together=('user','menuitem')
         
 class Order(models.Model):
-    user=models.ForeignKey(User,on_delete=modles.CASCADE)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
     delivery_crew=models.ForeignKey(User, on_delete=models.SET_NULL,null=True, related_name="delivery_crew")
     status=models.BooleanField(default=0, db_index=True)
-    total=DecimalField(max_digits=6,decimal_palces=2)
+    total=models.DecimalField(max_digits=6,decimal_places=2)
     date=models.DateField(db_index=True)
     
 class OrderItem(models.Model):
-    order=models.Foreignkey(User,on_delete=models.CASCADE)
+    order=models.ForeignKey(User,on_delete=models.CASCADE)
     menuitem=models.ForeignKey(MenuItems, on_delete=models.CASCADE)
     quantity=models.SmallIntegerField()
-    unit_price=models.DecimalField(max_digits=6,decimal_palces=2)
-    price=models.DecimalField(max_digits=6, decimal_palces=2)
+    unit_price=models.DecimalField(max_digits=6,decimal_places=2)
+    price=models.DecimalField(max_digits=6, decimal_places=2)
     
     class Meta:
-        unique_together('order','menu_item')
+        unique_together=('order','menuitem')
     
     
